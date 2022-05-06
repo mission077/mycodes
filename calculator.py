743ff2d590773ab4b26ccd704403d1511edca2f3
unknown <mission.kadariya@smail.astate.edu>
2022-05-05 20:16:39 -0500

    Calculator.py added to github

---
+++ calculator.py
@@ -0,0 +1,14 @@
+number1 = float(input("Enter the number: "))
+operator = input("Enter the opeartor: ")
+number2 = float(input("ENter the number"))
+
+if operator == "+":
+    print(number1 + number2)
+elif operator == "-":
+    print(number1 - number2)
+elif operator == "/":
+    print(number1 / number2)
+elif operator == "*":
+    print(number1 / number2)
+else:
+    print("INVALID SYNTAX!!!!")
